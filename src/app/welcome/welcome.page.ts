92a676e43484afaf07eecccfff5b9eb94d1e01e4
askribilel123 <askribilel09@gmail.com>
2024-06-10 10:56:30 +0100

    create first popup or alert

--- src/app/welcome/welcome.page.ts
+++ src/app/welcome/welcome.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-welcome',
@@ -7,9 +8,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./welcome.page.scss'],
 })
 export class WelcomePage implements OnInit {
-  constructor(private router: Router) {}
+  isModalOpen: boolean = false;
+  isLoading: boolean = true;
+  constructor(private router: Router) { }
 
   ngOnInit() {
     console.log('ngOninit welcome page');
+  }
+
+  setOpen(isOpen: boolean) {
+    this.isModalOpen = isOpen;
+  }
+
+  closeModal(isOpen: boolean) {
+    this.setOpen(isOpen);
+    setTimeout(() => {
+      this.router.navigate(['/login']);
+    }, 1000);
   }
 }
