1f85a8f5e994aa67695d710f71bf714b6233a2f0
askribilel123 <askribilel09@gmail.com>
2025-11-11 10:58:59 +0100

    create assistance form

--- src/app/assistance/assistance.module.ts
+++ src/app/assistance/assistance.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -12,9 +12,10 @@ import { AssistancePage } from './assistance.page';
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
-    AssistancePageRoutingModule
+    AssistancePageRoutingModule,
   ],
-  declarations: [AssistancePage]
+  declarations: [AssistancePage],
 })
 export class AssistancePageModule {}
--- src/app/assistance/assistance.page.ts
+++ src/app/assistance/assistance.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { IonModal } from '@ionic/angular';
 
 @Component({
   selector: 'app-assistance',
@@ -6,10 +8,55 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assistance.page.scss'],
 })
 export class AssistancePage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  @ViewChild(IonModal) modal!: IonModal;
+  reclamationForm: FormGroup;
+  tickets = [
+    {
+      id: '412587451',
+      subject: 'Problème de connexion',
+      fixedNumber: '0123456789',
+      creationDate: '2024-01-15',
+      subscriptionType: 'ADSL',
+      status: 'En attente',
+    },
+    {
+      id: '412587451',
+      subject: 'Problème de connexion',
+      fixedNumber: '0123456789',
+      creationDate: '2024-01-15',
+      subscriptionType: 'ADSL',
+      status: 'En attente',
+    },
+    {
+      id: '412587451',
+      subject: 'Problème de connexion',
+      fixedNumber: '0123456789',
+      creationDate: '2024-01-15',
+      subscriptionType: 'ADSL',
+      status: 'En attente',
+    },
+    {
+      id: '412587451',
+      subject: 'Problème de connexion',
+      fixedNumber: '0123456789',
+      creationDate: '2024-01-15',
+      subscriptionType: 'ADSL',
+      status: 'En attente',
+    },
+  ];
+  constructor() {
+    this.reclamationForm = new FormGroup({});
   }
 
+  ngOnInit() {}
+
+  /* Modal */
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  onWillDismiss(event: CustomEvent<any>) {}
+
+  onSubmit() {}
 }
